46f928616f32c4781e05cc5ecaf3c15a9811cbf4
NasrMan <linuxnasr@gmail.com>
2022-08-12 12:54:51 +0300

    add python

---
+++ CPP/_1DataType.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// global variable.
+
+ int a=12;
+    string uName="Nasr";
+    float f = 12.4333333;
+    double d = 12.3333333;
+
+main()
+{
+    // locaal variables, not seen outseide the function
+    // int a=12;
+    // string uName="Nasr";
+    // float f = 12.4333333;
+    // double d = 12.3333333;
+
+    cout<<"int a value is "<< a <<endl;
+    cout<<"Your name is " << uName <<endl;
+    cout<<"Your name is " << uName <<endl;
+    cout<< f <<endl;
+    cout<< d <<endl;
+}
