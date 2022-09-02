519713fdf136eeef05440db69c47251402d3365b
NasrMan <linuxnasr@gmail.com>
2022-09-02 16:35:59 +0300

    t

---
+++ kivy/rakwan.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+#from  Kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager,Screen
+
+class MyMainApp(App):
+    pass
+
+MyMainApp().run()
