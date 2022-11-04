fbd7db26d38d562be0077eec99eed6b3bbae965b
NasrMan <110780077+NasrMan@users.noreply.github.com>
2022-11-04 21:46:54 +0300

    Updates

---
+++ JavaFiles/arrayz.java
@@ -0,0 +1,30 @@
+package JavaFiles;
+
+public class arrayz {
+    public static void main(String[] args) {
+        
+        String [] cars = {"Toyota", "Nissan", "Fiat"};
+
+        System.out.println(cars.length);
+// Loop an array
+        for (String i : cars) {
+            System.out.println(i);
+          }
+
+// //         // Declare array elements
+        
+        // String [] carz = new String[2];
+        // carz[0]="Nissan";
+        // carz[1]="Toyota";
+        // carz[2]="kia";
+
+     
+
+        int [] arr={33,3,4,5};  
+        //printing array using for-each loop  
+        for(int z:arr)   System.out.println(z);  
+ 
+
+        
+    }
+}
